a6a5894b268a5be560a2f876bf6a3950f02acc37
Wandole <mchirez@gmail.com>
2022-11-28 01:00:12 +0100

    Add mobile menu. Add button and link state

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './stylesheets/main.scss'
+
+const btnMenu = document.querySelector('.mobile-menu')
+const menu = document.querySelector('.nav')
+const header = document.querySelector('header')
+
+const headerHeight = header.offsetHeight
+menu.style.top = headerHeight + 'px'
+
+const toggleMenu = (e) => {
+  e.preventDefault()
+  btnMenu.toggleAttribute('open')
+}
+
+btnMenu.onclick = toggleMenu
